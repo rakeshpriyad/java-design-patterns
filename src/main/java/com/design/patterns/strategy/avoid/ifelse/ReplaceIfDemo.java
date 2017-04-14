8d4adf9524bffb0712679acb0ecee500c93596f3
rakesh <rakeshpriyad@gmail.com>
2017-04-14 20:41:32 +0530

    strategy pattern remove if else with strategy

--- src/main/java/com/design/patterns/strategy/avoid/ifelse/ReplaceIfDemo.java
+++ src/main/java/com/design/patterns/strategy/avoid/ifelse/ReplaceIfDemo.java
@@ -4,37 +4,20 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ReplaceIfDemo {
-
-	public void replaceSwitchWithStrategyPattern() {
-		Map<MediaPlayerType, IMediaPlayer> strategies = new ConcurrentHashMap<MediaPlayerType, IMediaPlayer>();
-
-		IMediaPlayer pWMPlyer = new CWindowsMediaPlayer();
-		strategies.put(MediaPlayerType.WMPlayer, pWMPlyer);
-
-		IMediaPlayer pRMPlyer = new CRealMediaPlayer();
-		strategies.put(MediaPlayerType.RMPlayer, pRMPlyer);
-
-		IMediaPlayer pFMPlyer = new CFlashMediaPlayer();
-		strategies.put(MediaPlayerType.FMPlayer, pFMPlyer);
-
-		/////// Change the player based on media file extension
-		MediaPlayerType etype = MediaPlayerType.WMPlayer; // .WMV file
-		MediaPlayerType etype1 = MediaPlayerType.RMPlayer; // .WMV file
-		/*for (Map.Entry<MediaPlayerType, IMediaPlayer> entry : strategies.entrySet()) {
-			if (entry.getKey().equals(etype)) {
-				IMediaPlayer player = entry.getValue();
-				player.play();
-			}
-		}*/
-		
-		strategies.get(etype).play();
-		strategies.get(etype1).play();
-		
-		
+	private static Map<MediaPlayerType, IMediaPlayer> strategies = new ConcurrentHashMap<MediaPlayerType, IMediaPlayer>();
+	static {
+		strategies.put(MediaPlayerType.WMPlayer, new CWindowsMediaPlayer());
+		strategies.put(MediaPlayerType.RMPlayer, new CRealMediaPlayer());
+		strategies.put(MediaPlayerType.FMPlayer, new CFlashMediaPlayer());
 	}
-	
+
+	public void replaceSwitchWithStrategyPattern(MediaPlayerType etype) {
+		strategies.get(etype).play();
+	}
+
 	public static void main(String[] args) {
 		ReplaceIfDemo replaceIfDemo = new ReplaceIfDemo();
-		replaceIfDemo.replaceSwitchWithStrategyPattern();
+		replaceIfDemo.replaceSwitchWithStrategyPattern(MediaPlayerType.WMPlayer);
+		replaceIfDemo.replaceSwitchWithStrategyPattern(MediaPlayerType.RMPlayer);
 	}
 }
